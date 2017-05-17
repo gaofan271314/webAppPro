cd5e5e9142bb4bd6c0133e436b05d416e9798fc4
gaofan271314 <275501314@qq.com>
2017-05-17 11:05:09 +0800

    创建nav组件

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
 /**
  * Created by Administrator on 2017/5/16 0016.
  */
+(function (angular) {
+    "use strict"
+    /*创建模块*/
+    var app = angular.module("app",[])
+    /*创建控制器*/
+    app.controller("appController",['$scope',function ($scope) {
+        $scope.name = "趣闻一刻钟"
+    }])
+})(angular);
